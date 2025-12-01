27f255ed6aed26dba0012934651f5be5571c40ec
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-12-01 21:31:00 +0100

    Implementato termometro DHT11

--- src/devices/thermometer/thermometerDHT11.cpp
+++ src/devices/thermometer/thermometerDHT11.cpp
@@ -1,8 +1,27 @@
 #include "Arduino.h"
 #include "thermometerDHT11.h"
 
-ThermometerDHT11::ThermometerDHT11(uint8_t pin) : pin(pin) {
-    pinMode(pin, INPUT); // Inizializza pin analogico
+
+ThermometerDHT11::ThermometerDHT11(uint8_t pin) : pin(pin), dht(pin, DHTTYPE) {
+    dht.begin();
 }
 
+// restituisce temperatura in C
+float ThermometerDHT11::getTemperatureC() {
+    float temp = dht.readTemperature();
+    if (isnan(temp)) {
+        Serial.println("Errore lettura temperatura DHT11");
+        return NAN;
+    }
+    return temp;
+}
 
+// restituisce % umidità
+float ThermometerDHT11::getHumidity() {
+    float humidity = dht.readHumidity();
+    if (isnan(humidity)) {
+        Serial.println("Errore lettura umidità DHT11");
+        return NAN;
+    }
+    return humidity;
+}
--- src/devices/thermometer/thermometerDHT11.h
+++ src/devices/thermometer/thermometerDHT11.h
@@ -3,16 +3,20 @@
 
 #include "Arduino.h"
 #include "thermometer.h"
+#include <DHT.h>
+
+#define DHTTYPE DHT11  // Tipo di sensore (DHT11, DHT22, DHT21)
 
 class ThermometerDHT11 : public Thermometer {
 public:
     ThermometerDHT11(uint8_t pin);
     virtual float getTemperatureC() override;
     virtual void printC(float temp) override;
+    virtual float getHumidity();
 
 protected:
     uint8_t pin; // uint8_t evita di dover usare unsigned long long a causa del define A0
-    int read();
+    DHT dht;
 };
 
 #endif
