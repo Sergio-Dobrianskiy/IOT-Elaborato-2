948b84d41d5a01c08ed43371787b4597b4d3d780
Sergio Dobrianskiy <sergio.dobrianskiy@gmail.com>
2025-12-11 21:20:08 +0100

    Ordine config.h

--- src/config.h
+++ src/config.h
@@ -1,23 +1,24 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define LCD_LEN 16
-
-#define LG2 10
-#define LG1 9
-#define LR 6
-
-#define BTN1 5
-
-#define PIR 11
+#define TEMP A0
 
 #define SERVO 3
 
-#define TRIG 8
+#define BTN1 5
+
+#define LR 6
+#define LG1 9
+#define LG2 10
+
 #define ECHO 7
+#define TRIG 8
+
+#define PIR 11
+
+#define LCD_LEN 16
 
 #define DISTANCE_THRESHOLD 100
 
-#define TEMP A0
 
 #endif
